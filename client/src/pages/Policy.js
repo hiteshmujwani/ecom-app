a4b3fe2c2a38f1e3234f818df0f93e658537ec7d
Hitesh Mujwani <hiteshmujwaniofficial@gmail.com>
2024-01-24 16:43:21 +0530

    update policy

--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout/Layout'
 export default function Policy() {
   return (
     <Layout  tittle="Privacy & Policy">
-    <div>Policy</div>
+    <div>Policy and</div>
     </Layout>
   )
 }
